4bcc2af383fc180f03781cfdf10fa444bb3a1a23
POST3 PROG1 <post3.prog1@enetcom.rnu.tn>
2023-11-14 11:01:44 +0100

    message

---
+++ app/src/main/java/com/example/recycleview/Model/Word.java
@@ -0,0 +1,18 @@
+
+
+package com.example.recyclerview.model;
+public class Word {
+    private String mWord;
+
+    public Word(String word){
+        this.mWord = word;
+    }
+
+    public void setWord(String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+}
---
+++ app/src/main/java/com/example/recycleview/data/Datasource.java
@@ -0,0 +1,19 @@
+package com.example.recyclerview.data;
+
+import com.example.recyclerview.model.Word;
+
+import java.util.LinkedList;
+
+public class Datasource {
+
+    public Datasource(){}
+
+    public LinkedList<Word> loadWords(){
+        // Mettre les données initiales dans la liste de mots.
+        LinkedList<Word> wordList = new LinkedList<>();
+        for (int i = 0; i < 20; i++) {
+            wordList.addLast(new Word("Word " + i));
+        }
+        return wordList;
+    }
+}
